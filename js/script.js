46a88a3f1e704def39e1da2f33660a8217284d48
Flor Omurbekov <flor.omurbekov@mail.ru>
2022-03-10 23:47:04 +0300

    box-shadow

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const box = document.querySelector('.box')
+const shadowLeftRigth = document.querySelector('.left-right')
+const shadowUpDown = document.querySelector('.up-down')
+const shadowTransparency = document.querySelector('.transparency')
+const shadowSize = document.querySelector('.size')
+const shadowColor = document.querySelector('.color')
+const shadowOpasity = document.querySelector('.opasity')
+const generator = document.querySelector('.generator')
+const inset = document.querySelector('.inset')
+
+shadowLeftRigth.addEventListener('input', shadowGenerator)
+shadowUpDown.addEventListener('input', shadowGenerator)
+shadowTransparency.addEventListener('input', shadowGenerator)
+shadowSize.addEventListener('input', shadowGenerator)
+shadowColor.addEventListener('input', shadowGenerator)
+inset.addEventListener('input', shadowGenerator)
+
+function shadowGenerator() {
+    let insetValue = ''
+    if (inset.checked) {
+        insetValue = 'inset'
+    }
+    box.style.boxShadow = `${shadowLeftRigth.value}px ${shadowUpDown.value}px ${shadowTransparency.value}px ${shadowSize.value}px ${shadowColor.value} ${insetValue}`
+    generator.textContent = `box-shadow: ${shadowLeftRigth.value}px ${shadowUpDown.value}px ${shadowTransparency.value}px ${shadowSize.value}px ${shadowColor.value};`
+}
+
+shadowGenerator()
+
+
+
